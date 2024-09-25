fd69393d2f1e51f2cb6204057b9402c38367fef5
lucad5 <87230523+lucad5@users.noreply.github.com>
2024-09-24 21:25:06 -0300

    Move code into function

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -1,16 +1,24 @@
+
+#def print_current_line(possible_divisors):
+
+def determine_divisors_of_current_number(current_number, possible_divisors, divisors_of_current_number):
+
+    for divisor in possible_divisors:
+        if current_number % divisor == 0:
+            divisors_of_current_number.append(divisor)
+
+    return divisors_of_current_number
+
 possible_divisors = {}
 possible_divisors[3] = "fizz"
 possible_divisors[5] = "buzz"
 
+
 for i in range(1, 100):
-    print(i, end="")
+    print(i, end=" ")
 
     divisors_of_current_number = []
-
-    # See if any 
-    for divisor in possible_divisors:
-        if i % divisor == 0:
-            divisors_of_current_number.append(divisor)
+    divisors_of_current_number = determine_divisors_of_current_number(i, possible_divisors, divisors_of_current_number)
 
     divisors_of_current_number.sort()
 
@@ -19,7 +27,6 @@ for i in range(1, 100):
         continue
 
     else:
-        print(" ", end="")
         for divisor in divisors_of_current_number:
             print(possible_divisors[divisor], end="")
         print("\n")
