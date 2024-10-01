30328073191a4f1979c6538a117b42e0df8b0930
lucad5 <87230523+lucad5@users.noreply.github.com>
2024-10-01 20:41:54 -0300

    Add WIP code for HTML/JS fizzbuzz implementation

---
+++ javascript/fizzbuzz.js
@@ -0,0 +1,10 @@
+
+// TODO: to fill in this function
+
+function determine_fizzbuzz_words_for_each_number(fizzbuzz_numbers) {
+
+};
+
+const fizzbuzz_divisors_and_words = {3: "fizz", 5: "buzz"};
+
+document.getElementById("fizzbuzz_text").innerHTML = "test"
