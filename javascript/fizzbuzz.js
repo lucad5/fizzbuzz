c753db26ea78f333943114ae4a044433e7c14e71
lucad5 <87230523+lucad5@users.noreply.github.com>
2024-10-02 10:43:56 -0300

    Complete JS fizzbuzz implementation

--- javascript/fizzbuzz.js
+++ javascript/fizzbuzz.js
@@ -1,10 +1,24 @@
+function determine_fizzbuzz_words_for_each_number(fizzbuzz_numbers, fizzbuzz_range) {
 
-// TODO: to fill in this function
+    fizzbuzz_string = "";
+    for (i = fizzbuzz_range["min"]; i <= fizzbuzz_range["max"]; i++) {
 
-function determine_fizzbuzz_words_for_each_number(fizzbuzz_numbers) {
+        fizzbuzz_string = fizzbuzz_string.concat(i, " ");
 
+        for (let number in fizzbuzz_numbers) {
+            if (i % number === 0){
+                fizzbuzz_string = fizzbuzz_string.concat(fizzbuzz_numbers[number]);
+            };
+        };
+        fizzbuzz_string = fizzbuzz_string.concat("<br />");
+    };
+    console.log("fizzbuzz string: ", fizzbuzz_string)
+    return fizzbuzz_string;
 };
 
 const fizzbuzz_divisors_and_words = {3: "fizz", 5: "buzz"};
+const fizzbuzz_range = {"min": 1, "max": 100}
 
-document.getElementById("fizzbuzz_text").innerHTML = "test"
+determine_fizzbuzz_words_for_each_number(fizzbuzz_divisors_and_words, fizzbuzz_range)
+
+document.getElementById("fizzbuzz_text").innerHTML = determine_fizzbuzz_words_for_each_number(fizzbuzz_divisors_and_words, fizzbuzz_range)
