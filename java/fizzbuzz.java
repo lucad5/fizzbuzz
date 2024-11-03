646f75e023d098186992582641c2a6ce1aecf141
lucad5 <87230523+lucad5@users.noreply.github.com>
2024-11-03 17:40:09 -0400

    Complete Java implementation

--- java/fizzbuzz.java
+++ java/fizzbuzz.java
@@ -12,8 +12,6 @@ public class fizzbuzz {
         int range_max = range[1];
 
         for (int current_number = range_min; current_number <= range_max; current_number++) {
-            // TODO: Fill out this function
-            //    System.out.print(i);
 
             List<Integer> divisors_of_current_number = new ArrayList();
 
@@ -22,7 +20,6 @@ public class fizzbuzz {
                     divisors_of_current_number.add(divisor);
                 }
 
-            // TODO: add integers (i) in the range and the words for each integer to the array fizzbuzz_words
             }
 
             List word_array = new ArrayList<String>();
@@ -31,7 +28,6 @@ public class fizzbuzz {
                 
                 String[] words = divisors_and_words.get(divisor);
 
-//                for (int word_index; word_index < divisors_and_words.length; word_index++) {
                 for (String word : words) {
                     word_array.add(word);
                 }
@@ -39,13 +35,34 @@ public class fizzbuzz {
             }
 
             fizzbuzz_words.put(current_number, word_array);
-            System.out.print(fizzbuzz_words);
+//            System.out.print(fizzbuzz_words);
 
         }
 
         return fizzbuzz_words;
     }
 
+    static String convert_fizzbuzz_words_to_string(HashMap<Integer, List> fizzbuzz_numbers_and_words) {
+
+        String string_of_all_numbers_plus_words = "";
+
+        for (int number : fizzbuzz_numbers_and_words.keySet()) {
+            string_of_all_numbers_plus_words += number;
+
+            List<String> words_for_current_number = fizzbuzz_numbers_and_words.get(number);
+
+            if (words_for_current_number.size() != 0) {
+                string_of_all_numbers_plus_words += " ";
+                for (String word : words_for_current_number) {
+                    string_of_all_numbers_plus_words += word;
+                }    
+            }
+
+            string_of_all_numbers_plus_words += "\n";
+        }
+
+        return string_of_all_numbers_plus_words;
+    }
     public static void main(String[] args) {
         // TODO: modify the for loop to use maps
         
@@ -57,6 +74,7 @@ public class fizzbuzz {
         HashMap<Integer, List> fizzbuzz_numbers_and_words = new HashMap<>();
         fizzbuzz_numbers_and_words = determine_fizzbuzz_words(range, divisors_and_words);
 
+        System.out.print(convert_fizzbuzz_words_to_string(fizzbuzz_numbers_and_words));
 //        String[] words = {"fizz"};
 //        numbers_and_words.put(1, words);
 //        String[] word = numbers_and_words.get(1);
